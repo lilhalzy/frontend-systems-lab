5e4b7284b14a0ec9f09b9819a4719bca994c7bb7
lilhalzy <lilhalzy11@gmail.com>
2026-05-07 13:01:58 +0800

    feat: create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return(
+        <header>
+            <h2>This is my journey on remembering all those syntaxes and coding flows</h2>
+        </header>
+    )
+}
+
+export default Header
