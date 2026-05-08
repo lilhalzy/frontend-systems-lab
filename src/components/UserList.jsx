da3ae33762f85c8e3da9eec28b3969366fe4f957
lilhalzy <lilhalzy11@gmail.com>
2026-05-08 12:18:33 +0800

    feat:handle async loading and error states

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react"
 
 const UserList = () => {
     const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -20,6 +22,42 @@ const UserList = () => {
     
         fetchUsers()
     }, [])
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                setLoading(true)
+
+                const response = await fetch(
+                    'https://jsonplaceholder.typicode.com/invalid-url'
+                )
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch users')
+                }
+
+                const data = await response.json()
+
+                setUsers(data)
+            }   catch (err) {
+                setError(err.message)
+            }   finally {
+                setLoading(false)
+            }
+        }
+
+        fetchUsers()
+    }, [])
+
+    if (loading) {
+        return <p>Loading users...</p>
+    }
+    if (error) {
+        return <p>{error}</p>
+    }
+    if (users.length === 0) {
+        return <p>No users found</p>
+    }
 
     return (
         <section>
